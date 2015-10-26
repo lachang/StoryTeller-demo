4ee8ea979b8e9b0fa5c3961d28791641543bea65
Arvind Gopalakrishnan <arvindzin@gmail.com>
2015-10-25 23:25:17 -0700

    Change S2 bucket name to storyteller-demo

--- storyteller-ios/Configuration.swift
+++ storyteller-ios/Configuration.swift
@@ -15,6 +15,6 @@ struct Configuration {
     let CognitoRegionType = AWSRegionType.USEast1
     let CognitoIdentityPoolId = "us-east-1:371de83b-8ff2-4ea9-b184-9c33dfdfa37d"
     let S3Domain = "https://s3.amazonaws.com/"
-    let S3BucketName = "storyteler"
+    let S3BucketName = "storyteller-demo"
 }
 let config = Configuration()
